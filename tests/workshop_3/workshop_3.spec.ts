75324300dadacc772f101912b831de0b4e8d4ea1
Nguyen Thanh Sang <nguyenthanhsang20012017@gmail.com>
2024-12-13 17:19:56 +0700

    Test handling alert, confirm alert, pop-up

--- tests/workshop_3/workshop_3.spec.ts
+++ tests/workshop_3/workshop_3.spec.ts
@@ -56,16 +56,62 @@ test("Test create an account in Palmer", async ({ page }) => {
   await page.waitForTimeout(3000); // Hoan viec test 3s
 });
 
-test.only("Test add to cart in Palmer", async ({ page }) => {
+test("Test add to cart in Palmer", async ({ page }) => {
   await page.goto("https://www.palmers.com/");
   // await page.locator(`//input[@name='pla_quantity_325']`).fill("3");  //Option 1
-  await page.locator(`//article[contains(@class, '325')]//div[contains(@class, 'pla-qty-plus')][normalize-space()='+']`).click({button: "left", clickCount: 3});  //Option 2
-  await page.locator(`//article[contains(@data-id-product,'325')]//a[@title='Add to cart']`).click();
+  await page
+    .locator(
+      `//article[contains(@class, '325')]//div[contains(@class, 'pla-qty-plus')][normalize-space()='+']`
+    )
+    .click({ button: "left", clickCount: 3 }); //Option 2
+  await page
+    .locator(
+      `//article[contains(@data-id-product,'325')]//a[@title='Add to cart']`
+    )
+    .click();
   await page.locator(`//button[normalize-space()='CONTINUE SHOPPING']`).click();
   await page.locator(`//span[contains(@id,'attribute_7_4-container')]`).click();
   await page.locator(`//li[normalize-space()='3.5 oz']`).click();
-  await page.locator(`//article[contains(@class, 'pla_block pla_block_7')]//div[contains(@class, 'pla-qty-plus')][normalize-space()='+']`).click({button: "left", clickCount: 1});
-  await page.locator(`//article[contains(@class, 'pla_block pla_block_7')]//a[@title='Add to cart']`).click();
+  await page
+    .locator(
+      `//article[contains(@class, 'pla_block pla_block_7')]//div[contains(@class, 'pla-qty-plus')][normalize-space()='+']`
+    )
+    .click({ button: "left", clickCount: 1 });
+  await page
+    .locator(
+      `//article[contains(@class, 'pla_block pla_block_7')]//a[@title='Add to cart']`
+    )
+    .click();
   await page.locator(`//button[normalize-space()='CONTINUE SHOPPING']`).click();
   await page.waitForTimeout(3000); // Hoan viec test 3s
 });
+
+test("Advanced Interactions", async ({ page }) => {
+  // Xu ly interaction hover
+  await page.goto(
+    "file:///C:/Users/sang.nguyen/Documents/Thuc_tap_IDTEK/Palmer_Gitlab_Test/tests/workshop_3/index.html"
+  );
+  await page.locator(`//button[contains(@id,'hover')]`).hover();
+  expect(await page.textContent("button#hover-me")).toContain("Text Changed!"); // Kiểm tra xem text của button có bị thay đổi thành Text Changed khi hover qua không
+
+  // Xu ly interaction right click
+  await page
+    .locator(`//button[contains(@id,'context')]`)
+    .click({ button: "right" });
+  expect(await page.getByText("Context Menu Appears!").textContent()).toContain(
+    "Context Menu Appears!"
+  ); // Kiểm tra xem khi click chuột phải vào button có xuất hiện chữ Context Menu Appears! không
+
+  // Xu ly interaction double click
+  await page.locator(`//button[contains(@id,'double')]`).dblclick();
+  expect(await page.locator("img").count()).toBe(1); // Kiểm tra xem khi double click vào button có xuất hiện 1 hình ảnh hay không
+});
+
+test('Drag and Drop', async ({page}) => { 
+    // Xu ly interation drag and drop
+  await page.goto(
+    "file:///C:/Users/sang.nguyen/Documents/Thuc_tap_IDTEK/Palmer_Gitlab_Test/tests/workshop_3/index.html"
+  );
+  await page.dragAndDrop('.drag-source', '.drop-target');
+  expect(await page.textContent('.drop-target')).toContain("Success");
+ })
---
+++ tests/workshop_4/workshop_4.spec.ts
@@ -0,0 +1,42 @@
+import { expect, test } from "playwright/test";
+
+test("Handling Alerts", async ({ page }) => {
+  await page.goto(
+    "file:///C:/Users/sang.nguyen/Documents/Thuc_tap_IDTEK/Palmer_Gitlab_Test/tests/workshop_4/index.html"
+  );
+  let allertMessage = "";
+  page.on("dialog", async (dialog) => {
+    expect(dialog.type()).toBe("alert"); // Kiểm tra loại của dialog là alert
+    allertMessage = await dialog.message(); // Lấy message của dialog và bỏ vào biến allertMesage
+    await dialog.accept(); // Click nút OK khi dialog xuất hiện
+  });
+  await page.click("#show-alert");
+  expect(allertMessage).toBe("This is a simple alert."); // Kiểm tra xem thông điệp của alert có đúng là "This is a simple alert." hay không
+});
+
+test("Confirm Alert", async ({ page }) => {
+  await page.goto(
+    "file:///C:/Users/sang.nguyen/Documents/Thuc_tap_IDTEK/Palmer_Gitlab_Test/tests/workshop_4/index.html"
+  );
+  let allertMessage = "";
+  page.on("dialog", async (dialog) => {
+    allertMessage = dialog.message(); // Lấy message của dialog và bỏ vào biến allertMesage
+    await page.waitForTimeout(4000);
+    await dialog.dismiss(); // Click nút cancel khi dialog xuất hiện
+  });
+  await page.click("#show-confirm");
+  expect(allertMessage).toBe("You clicked Cancel."); // Kiểm tra xem thông điệp của alert có đúng là "You clicked Cancel." hay không
+});
+
+test("Handling pop-up", async ({ page }) => {
+  await page.goto(
+    "file:///C:/Users/sang.nguyen/Documents/Thuc_tap_IDTEK/Palmer_Gitlab_Test/tests/workshop_4/index.html"
+  );
+  const [popup] = await Promise.all([  // Chờ đồng thời cả hai hành động: đợi xuất hiện pop-up và nhấp vào nút để mở pop-up.
+    page.waitForEvent("popup"),  // Đợi sự kiện pop-up xuất hiện
+    page.click("#open-popup"), 
+  ]);
+
+  await popup.waitForLoadState(); // Chờ pop-up load xong
+  await popup.close();  // Đóng pop-up
+});
